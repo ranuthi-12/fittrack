cdec2d6e4c05d8b61284a3b4143f1d9310a20c66
Ranuthi Dahamsa <ranuthidahamsa12@gmail.com>
2026-07-13 12:26:40 +0530

    feat: setup project structure and routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
