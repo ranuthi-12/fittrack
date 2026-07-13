93851492ad1d6e0eb6a6dded341f4a3b312853d7
Ranuthi Dahamsa <ranuthidahamsa12@gmail.com>
2026-07-13 13:15:01 +0530

    feat: add register page with membership plan selection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.jsx";
 import Login from "./pages/Login.jsx";
+import Register from './pages/Register.jsx';
 
 import './App.css'
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
